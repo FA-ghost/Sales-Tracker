77af9da29323bc57aaeb9569aa28d935cf3847a8
FA-ghost <fisayoaasa@outlook.com>
2025-08-12 17:18:37 +0100

    Feat: Create Navbar and Sidebar with responsive adoption

--- sales-tracker-frontend/src/components/Navbar.jsx
+++ sales-tracker-frontend/src/components/Navbar.jsx
@@ -10,21 +10,21 @@ function Navbar(){
 
     return (
         <nav className="flex p-[10px] justify-end relative">
-            <div className="flex gap-[15px] p-[5px] items-center rounded-sm hover:bg-[#B2B0E8]/70 hover:text-white" onClick={updateIsOpen}>
+            <div className="flex gap-[15px] p-[5px] items-center rounded-sm hover:bg-[#3B38A0]/80 hover:text-white" onClick={updateIsOpen}>
                 <img src={profilePic} alt="user profile picture" className="w-[40px] h-[40px] rounded-full"/>
                 <span>Manager-FA</span>
-                <div className={`w-[20px] h-[20px] rounded-full shadow-sm ${isOpen ? "rotate-180" : "rotate-0"} transition-all duration-100 ease-in-out`}>
+                <div className={`w-[20px] h-[20px] rounded-full shadow-sm ${isOpen ? "rotate-180" : "rotate-0"} transition-all duration-300 ease-in-out`}>
                     <ChevronDown size={20} />
                 </div>
             </div>
 
-            <div className={`w-[120px] bg-[#B2B0E8] rounded-md absolute top-[70px] right-[30px] ${isOpen ? "block" : "hidden"} `} onMouseLeave={updateIsOpen}>
+            <div className={`w-[150px] bg-[#3B38A0]/80 text-white rounded-md absolute top-[70px] right-[30px] ${isOpen ? "block" : "hidden"} `} onMouseLeave={updateIsOpen}>
                 <ul className="w-full flex flex-col gap-[5px] my-[5px]">
-                    <li className="flex gap-[10px] items-center w-full p-[10px] hover:bg-gray-100">
+                    <li className="flex gap-[10px] items-center w-full p-[10px] hover:bg-gray-200/80 hover:text-black">
                         <UserRoundPen size={20} />
                         <span>Profile</span>
                     </li>
-                    <li className="flex gap-[10px] items-center w-full p-[10px] hover:bg-gray-100">
+                    <li className="flex gap-[10px] items-center w-full p-[10px] hover:bg-gray-200/80 hover:text-black">
                         <LogOut size={20} />
                         <span>Signout</span>
                     </li>
--- sales-tracker-frontend/src/pages/Home.jsx
+++ sales-tracker-frontend/src/pages/Home.jsx
@@ -1,16 +1,25 @@
 import React from "react";
+import { useState } from "react";
 import Navbar from "../components/Navbar.jsx";
 import Sidebar from "../components/Sidebar.jsx";
 
 function Home(){
+    const [isOpen, setIsOpen] = useState(true);
+
+    const updateIsOpen = () => setIsOpen(!isOpen);
+
     return (
         <>
-            <div className="grid grid-cols-[1fr_4fr] grid-rows-[75px_4fr] gap-[15px]">
+            <div className="grid grid-rows-[75px_4fr] gap-[15px] transition-[grid-template-columns] duration-300 ease-in-out"
+            style={{
+                        gridTemplateColumns: isOpen ? "250px 1fr" : "70px 1fr",
+                    }}
+            >
                 <div className="col-start-2 row-start-1">
                     <Navbar />
                 </div>
                 <div className="w-full h-[100vh] col-start-1 row-start-1 row-span-full">
-                    <Sidebar />
+                    <Sidebar update={updateIsOpen} isOpen={isOpen} />
                 </div>
                 <div className="col-start-2 row-start-2">
                     teststsstst
